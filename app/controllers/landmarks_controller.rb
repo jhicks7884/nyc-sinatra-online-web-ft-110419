606aec9174ae57938096283f46b0f9f6ea4cd791
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-15 15:25:29 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,37 @@
 class LandmarksController < ApplicationController
-  # add controller methods
-end
+
+    get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :'landmarks/index'
+    end
+
+    #new
+    get '/landmarks/new' do
+      erb :'landmarks/new'
+    end
+
+    post '/landmarks/new' do
+      @landmark = Landmark.new(params[:name], params[:year])
+      @landmark.save
+    end
+
+    #show
+    get '/landmarks/:id' do
+      @landmark = Landmark.find_by_id(params[:id])
+      erb :'landmarks/show'
+    end
+
+    #edit
+    get '/landmarks/:id/edit' do
+      @landmark = Landmark.find_by_id(params[:id])
+      erb :'landmarks/edit'
+    end
+
+    post '/landmarks/:id/edit' do
+      @landmark = Landmark.find_by_id(params[:id])
+      @landmark.name = params[:name]
+      @landmark.year_completed = params[:year_completed]
+      @landmark.save
+      redirect "/landmarks/#{params[:id]}"
+    end
+  end
